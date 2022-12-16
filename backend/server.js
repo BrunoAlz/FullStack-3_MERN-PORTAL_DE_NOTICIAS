effdb38bae5fe56c5dab2375ea749760b2a64085
Bruno Alves <bruno6821@gmail.com>
2022-12-16 19:43:30 -0300

    Conexão com o Banco Realizada - OK

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const app = express();
-
 require("dotenv").config();
 const port = process.env.PORT;
 
+const express = require("express");
+const app = express();
+
+// Configurando a Conexão com o BD
+const databaseConnect = require("./src/database/db");
+
+// Configuração das Respostas em JSON e Form data
 app.use(express.json());
 
 const router = require("./src/routes/Routes");
--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose").set("strictQuery", false);
+
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
@@ -6,7 +7,7 @@ const dbPassword = process.env.DB_PASS;
 const databaseConnect = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ul4mvju.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${dbUser}:${dbPassword}@noticias.orjzglr.mongodb.net/?retryWrites=true&w=majority`
     );
 
     console.log("Conectou ao Banco!");
@@ -19,4 +20,3 @@ const databaseConnect = async () => {
 databaseConnect();
 
 module.exports = databaseConnect;
-
