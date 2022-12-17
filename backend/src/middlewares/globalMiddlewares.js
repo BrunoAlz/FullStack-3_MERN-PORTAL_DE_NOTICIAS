d24006bd1cc376307003210cdfa5afdc434da320
Bruno Alves <bruno6821@gmail.com>
2022-12-17 00:08:33 -0300

    Implementação inicial dos Middlewares Globais.

---
+++ backend/src/middlewares/globalMiddlewares.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const userService = require("../services/userService");
+
+const validateIdMiddleware = (req, res, next) => {
+  // 1- Recupera o Id passado na Requisição
+  const id = req.params.id;
+  // 2 Verifica se o Id da está correto
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ errors: ["Este usuário não existe."] });
+  }
+};
+
+const validateUserMiddleware = async (req, res, next) => {
+  // 1- Recupera o Id passado na Requisição
+  const id = req.params.id;
+  // 2- Verifica se o Usuário existe
+  const user = await userService.getUserByIdService(id);
+  if (!user) {
+    return res.status(404).json({ errors: ["Este usuário não existe."] });
+  }
+};
+
+module.exports = {
+  validateIdMiddleware,
+  validateUserMiddleware,
+};
