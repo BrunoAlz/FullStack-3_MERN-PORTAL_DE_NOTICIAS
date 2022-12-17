42c5913a0ab9f0d1189c054c0469f9e85e9758c4
Bruno Alves <bruno6821@gmail.com>
2022-12-17 18:30:58 -0300

    Refactor: Envolvendo as Requisições no try - catch

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,87 +2,102 @@ const userService = require("../services/userService");
 const mongoose = require("mongoose");
 
 const createUserController = async (req, res) => {
-  // 1- Extrai os dados do corpo da Requisição
-  const {
-    name,
-    username,
-    email,
-    password,
-    profileImage,
-    backgroundImage,
-    about,
-  } = req.body;
-
-  // 3- E armazena o retorno em newUser // 2- Manda os dados para o Service
-  const newUser = await userService.createUserService(req.body);
-
-  // 4- Se o usuário não for criado por erros no servidor retorna o erro
-  if (!newUser) {
-    res
-      .status(422)
-      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
-    return;
-  }
-
-  // 5- Se o usuário for criado, retorna o status, a mensagem e os dados
-  res.status(201).send({
-    message: "Usuário criado com sucesso!",
-    user: {
-      id: newUser._id,
+  try {
+    // 1- Extrai os dados do corpo da Requisição
+    const {
       name,
       username,
       email,
+      password,
       profileImage,
       backgroundImage,
       about,
-    },
-  });
+    } = req.body;
+
+    // 3- E armazena o retorno em newUser // 2- Manda os dados para o Service
+    const newUser = await userService.createUserService(req.body);
+
+    // 4- Se o usuário não for criado por erros no servidor retorna o erro
+    if (!newUser) {
+      res
+        .status(422)
+        .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+      return;
+    }
+
+    // 5- Se o usuário for criado, retorna o status, a mensagem e os dados
+    res.status(201).send({
+      message: "Usuário criado com sucesso!",
+      user: {
+        id: newUser._id,
+        name,
+        username,
+        email,
+        profileImage,
+        backgroundImage,
+        about,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 const getAllUsersController = async (req, res) => {
-  const users = await userService.getAllUsersService();
+  try {
+    const users = await userService.getAllUsersService();
 
-  res.status(200).send(users);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 const getUserByIdController = async (req, res) => {
-  // 1- Recupera o usuário passado pelo Middleware
-  const user = req.user;
-  
-  // 2-  retorna a response para o front 
-  res.send(user);
+  try {
+    // 1- Recupera o usuário passado pelo Middleware
+    const user = req.user;
+
+    // 2-  retorna a response para o front
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 const updateUserController = async (req, res) => {
-  // 1- Recupera o Id na requisição, passado pelo Middleware
-  const {id, user} = req;
-  console.log(id, user);
-  // 2 - Recupera os dados da Requisição
-  const {
-    name,
-    username,
-    email,
-    password,
-    profileImage,
-    backgroundImage,
-    about,
-  } = req.body;
+  try {
+    // 1- Recupera o Id na requisição, passado pelo Middleware
+    const { id, user } = req;
+    // 2 - Recupera os dados da Requisição
+    const {
+      name,
+      username,
+      email,
+      password,
+      profileImage,
+      backgroundImage,
+      about,
+    } = req.body;
 
-  // 3- O Middleware valida o Id aqui no meio da Requisição
+    // 3- O Middleware valida o Id aqui no meio da Requisição
 
-  // 4- Apos a validação pelo middleware os dados é passado apra o service
-  await userService.updateUserService(
-    id,
-    name,
-    username,
-    email,
-    password,
-    profileImage,
-    backgroundImage,
-    about
-  );
+    // 4- Apos a validação pelo middleware os dados é passado apra o service
+    await userService.updateUserService(
+      id,
+      name,
+      username,
+      email,
+      password,
+      profileImage,
+      backgroundImage,
+      about
+    );
 
-  res.status(200).send({ message: "Usuário atualizado com Sucesso!" });
+    res.status(200).send({ message: "Usuário atualizado com Sucesso!" });
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 // 1°
--- backend/src/middlewares/globalMiddlewares.js
+++ backend/src/middlewares/globalMiddlewares.js
@@ -2,29 +2,37 @@ const mongoose = require("mongoose");
 const userService = require("../services/userService");
 
 const validateIdMiddleware = (req, res, next) => {
-  // 1- Recupera o Id passado na Requisição
-  const id = req.params.id;
-  // 2 Verifica se o Id da está correto
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ errors: ["Id de usuário incorreto."] });
+  try {
+    // 1- Recupera o Id passado na Requisição
+    const id = req.params.id;
+    // 2 Verifica se o Id da está correto
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ errors: ["Id de usuário incorreto."] });
+    }
+    next();
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
   }
-  next();
 };
 
 const validateUserMiddleware = async (req, res, next) => {
-  // 1- Recupera o Id passado na Requisição
-  const id = req.params.id;
-  // 2- Verifica se o Usuário existe
-  const user = await userService.getUserByIdService(id);
-  if (!user) {
-    return res.status(404).json({ errors: ["Este usuário não existe."] });
-  }  
-  // Guarda os dados buscados na requisição atual e passa para a próxima
-  // evitando que seja necessário buscar esses dados novamente.
-  req.id = id;
-  req.user = user;
+  try {
+    // 1- Recupera o Id passado na Requisição
+    const id = req.params.id;
+    // 2- Verifica se o Usuário existe
+    const user = await userService.getUserByIdService(id);
+    if (!user) {
+      return res.status(404).json({ errors: ["Este usuário não existe."] });
+    }
+    // Guarda os dados buscados na requisição atual e passa para a próxima
+    // evitando que seja necessário buscar esses dados novamente.
+    req.id = id;
+    req.user = user;
 
-  next();
+    next();
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 module.exports = {
