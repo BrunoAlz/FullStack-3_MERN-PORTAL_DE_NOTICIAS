7c3f7f12b5f0b346d596ae2069666885081db5dd
Bruno Alves <bruno6821@gmail.com>
2022-12-17 20:12:18 -0300

    Instalando o Bcrypt e Criptografando a senha do usuário

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 // Criando o SCHEMA de USER, Serve como blueprint para o Model
 const userSchema = new Schema(
@@ -7,13 +8,19 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     profileImage: { type: String, required: true },
     backgroundImage: { type: String, required: true },
     about: { type: String, required: false },
   },
   { timestamps: true }
 );
+
+// Criptografando a senha do usuário no Pré save do Model
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 // Criando o model no MongoDB
 const User = mongoose.model("User", userSchema);
