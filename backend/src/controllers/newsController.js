9556b49845c908907fd190f476ecc489f9859fbe
Bruno Alves <bruno6821@gmail.com>
2022-12-20 07:12:44 -0300

    Implementando o Controller para criar os POSTS

--- backend/src/controllers/newsController.js
+++ backend/src/controllers/newsController.js
@@ -1,9 +1,30 @@
-const postCreateController = (req, res) => {
-  res.send("cria post ok");
+const newsService = require("../services/newsService");
+
+const postCreateController = async (req, res) => {
+  try {
+    const { title, body, banner } = req.body;
+
+    if (!title || !banner || !body) {
+      res.status(400).send({ errors: "Preencha todos os campos" });
+    }
+
+    await newsService.postCreateService({
+      title,
+      body,
+      banner,
+      id: "1"
+    })
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 const postGetAllController = (req, res) => {
-  res.send("Lista todos os posts ok");
+  try {
+    res.send("lista post ok");
+  } catch (error) {
+    res.status(500).send({ errors: error.message });
+  }
 };
 
 module.exports = {
