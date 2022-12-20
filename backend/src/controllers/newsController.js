6f9361bb868fb86a00925fda7cefb0427dc5f460
Bruno Alves <bruno6821@gmail.com>
2022-12-20 07:14:05 -0300

    Implementando a Paginação - OK

--- backend/src/controllers/newsController.js
+++ backend/src/controllers/newsController.js
@@ -23,25 +23,62 @@ const postCreateController = async (req, res) => {
 
 const postGetAllController = async (req, res) => {
   try {
+    // Recupera os parametros da URL
     let { limit, offset } = req.query;
-
+    // Converte para Inteiro
     limit = Number(limit);
     offset = Number(offset);
-
+    // Se não tiver parametros seta o padrão
     if (!limit) {
-      limit = 10;
+      limit = 5;
     }
-
+    // Se não tiver parametros seta o padrão
     if (!offset) {
       offset = 0;
     }
 
+    // faz a query, passando os parametros
     const news = await newsService.postGetAllService(offset, limit);
+    // faz a query para recuperar o total de posts
+    const total = await newsService.postsCountService();
+    // recupera a url atual
+    const currentUrl = req.baseUrl;
+
+    // monta a nova URL com a paginação dinâmica
+    const next = offset + limit;
+    nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    // monta a Url anterior com a paginação anterior
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
     if (news.lenght === 0) {
       return res.status(400).send({ errors: "Nehuma postagem até o momento" });
     }
 
-    res.status(200).send(news);
+    res.status(200).send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+
+      resuts: news.map((newsItem) => ({
+        id: newsItem._id,
+        title: newsItem.title,
+        text: newsItem.text,
+        banner: newsItem.banner,
+        likes: newsItem.likes,
+        comments: newsItem.comments,
+        name: newsItem.user.name,
+        userName: newsItem.user.username,
+        profileImage: newsItem.user.profileImage,
+      })),
+    });
   } catch (error) {
     res.status(500).send({ errors: error.message });
   }
--- backend/src/services/newsService.js
+++ backend/src/services/newsService.js
@@ -3,9 +3,12 @@ const News = require("../models/News");
 const postCreateService = (body) => News.create(body);
 
 const postGetAllService = (offset, limit) =>
-  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+
+const postsCountService = () => News.countDocuments();
 
 module.exports = {
   postCreateService,
   postGetAllService,
+  postsCountService,
 };
