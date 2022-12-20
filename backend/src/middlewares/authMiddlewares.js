be2b44e06aafe311155e67e4a5a1a79782830547
Bruno Alves <bruno6821@gmail.com>
2022-12-20 08:01:19 -0300

    Adicionando Autorização no Endpoint de Create Post

--- backend/src/middlewares/authMiddlewares.js
+++ backend/src/middlewares/authMiddlewares.js
@@ -1,39 +1,47 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const token = process.env.JWT_SECRET;
+const jwt_secret = process.env.JWT_SECRET;
+
+const userService = require("../services/userService");
 
 const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
-try {
-	
-	  if (!authorization) {
-	    return res.status(401);
-	  }
-	
-	  const parts = authorization.split(" ");
-	
-	  if (parts.length !== 2) {
-	    return res.send(401);
-	  }
-	
-	  const [schema, token] = parts;
-	
-	  if (schema !== "Bearer") {
-	    return res.send(401);
-	  }
-	
-	  jwt.verify(token, token, (error, decoded) => {
+  try {
+    if (!authorization) {
+      return res.status(401);
+    }
+
+    const parts = authorization.split(" ");
+
+    if (parts.length !== 2) {
+      return res.send(401);
+    }
+
+    const [schema, token] = parts;
+
+    if (schema !== "Bearer") {
+      return res.send(401);
+    }
+
+    jwt.verify(token, jwt_secret, async (error, decoded) => {
       if (error) {
-        return res.status(401).send({errors: "Token Inválido"})
+        return res.status(401).send({ errors: "Token Inválido" });
       }
-	  })
-	
-	  next();
-} catch (error) {
-  return res.send(500);	
-}
+
+      const user = await userService.getUserByIdService(decoded.id);
+      if (!user || !user.id) {
+        return res.status(401).send({ errors: "Token Inválido" });
+      }
+
+      req.userId = user;
+    });
+
+    next();
+  } catch (error) {
+    return res.send(500);
+  }
 };
 
 module.exports = {
-  authMiddleware
-}
+  authMiddleware,
+};
