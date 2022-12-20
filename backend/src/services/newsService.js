65b7c70484f96c02c336d744ae27800278545b2f
Bruno Alves <bruno6821@gmail.com>
2022-12-20 07:37:37 -0300

    Adicionanndo a Paginação no Post Service

--- backend/src/services/newsService.js
+++ backend/src/services/newsService.js
@@ -2,7 +2,8 @@ const News = require("../models/News");
 
 const postCreateService = (body) => News.create(body);
 
-const postGetAllService = () => News.find();
+const postGetAllService = (offset, limit) =>
+  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
 
 module.exports = {
   postCreateService,
