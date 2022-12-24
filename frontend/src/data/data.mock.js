79a50b909bb0b58966cbb3fd98edcce329b52f9f
Bruno Alves <bruno6821@gmail.com>
2022-12-24 11:19:44 -0300

    Criando os Dados mockados

---
+++ frontend/src/data/data.mock.js
@@ -0,0 +1,78 @@
+export const news = [
+  {
+    title: "Lula está em São Paulo para o Natal; PF faz segurança do petista",
+    text: "O presidente diplomado, Luiz Inácio Lula da Silva (PT), está em uma casa no bairro do Alto de Pinheiros, em São Paulo, onde passará o Natal. Agentes da Polícia Federal fazem a segurança da rua, que, até agora, não tem bloqueios. Eles solicitaram a identif",
+    banner:
+      "https://conteudo.imguol.com.br/c/noticias/d0/2022/12/20/lula-1671567888494_v2_615x300.jpg",
+  },
+
+  {
+    title:
+      "Bolsonaro sanciona projeto de lei que libera R$ 31,5 milhões para a retomada da emissão de passaportes",
+    text: "Segundo a Polícia Federal, cerca de 100 mil pessoas estão na fila de espera em todo o país.",
+    banner:
+      "https://s2.glbimg.com/pXs3Qe7OaViTv8XWJzT6tjHnKgE=/1200x/smart/filters:cover():strip_icc()/s03.video.glbimg.com/x720/11228890.jpg",
+  },
+
+  {
+    title:
+      "Aeronautas suspendem greve até domingo para votar nova proposta das empresas",
+    text: "Suspensão da paralisação vale das 6h deste sábado (24) até meio-dia de domingo (25). O resultado da votação da categoria será divulgado às 12h30 de domingo. Movimento começou na segunda (19).",
+    banner:
+      "https://s2.glbimg.com/16cblJDg2HA7uUD5tJZAwmVpGKA=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/7/w/wgC0NlRK2pDOiXyYskrg/fup20221219162.jpg",
+  },
+
+  {
+    title:
+      'Deputados da Bancada Evangélica votam contra Bolsa Família e são criticados: "pastores da fome" - TV Fórum',
+    text: '<ol><li>Deputados da Bancada Evangélica votam contra Bolsa Família e são criticados: "pastores da fome"  TV Fórum\r\n</li><li>PEC da Transição é aprovada, o que deve mudar no próximo ano?  PronaTEC\r\n</li><li>Aprovados PEC da Transição e Orçamento para 2023 - Rá…',
+    banner: null,
+  },
+
+  {
+    title:
+      "Análise: Lula teve reuniões com Simone Tebet e Marina Silva | WW - CNN Brasil",
+    text: "<ol><li>Análise: Lula teve reuniões com Simone Tebet e Marina Silva | WW  CNN Brasil\r\n</li><li>Marina Silva deve assumir Ministério do Meio Ambiente; Lula encontra Tebet e discutem cargo  Rádio BandNews FM\r\n</li><li>Marina recusa convite para ser a Autoridade…",
+    url: "https://news.google.com/__i/rss/rd/articles/CBMiK2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9YkhVS2hzREREdlnSAQA?oc=5",
+    banner: null,
+  },
+
+  {
+    title:
+      "Greve dos aeronautas é suspensa, e nova votação definirá rumos do movimento",
+    text: "Para definir se a paralisação vai ou não continuar, uma nova votação será iniciada neste sábado (24/12), a partir das 6h",
+    banner:
+      "https://i.em.com.br/G1cO7yKmtJ_qxDfZz-k-6UuI9Oo=/600x315/smart/imgsapp.em.com.br/app/noticia_127983242361/2022/12/23/1436969/greve-dos-aeronautas-atrasou-voos-em-confins-as-vesperas-do-natal_1_59886.jpg",
+  },
+
+  {
+    title: "Aras vai apresentar ação contra indulto de Natal de Bolsonaro",
+    text: "Decreto do presidente perdoou policiais condenados pelo massacre do Carandiru, em que 111 presos foram mortos em 1992. Leia no Poder360.",
+    banner:
+      "https://static.poder360.com.br/2022/03/pgr-augusto-aras-milton-ribeiro-848x477.jpg",
+  },
+
+  {
+    title:
+      "Sobe para 200 milhões o número de pessoas em alerta nos EUA por causa de tempestade de inverno",
+    text: "O ciclone bomba Elliot cobre boa parte do território americano e derrubou a temperatura no país. Em Bismarck, na Dakota do Norte, os termômetros marcaram -55ºC.",
+    banner:
+      "https://s2.glbimg.com/OT4nyA5TaQUpkkHVdIskhnbNuys=/1200x/smart/filters:cover():strip_icc()/s03.video.glbimg.com/x720/11228758.jpg",
+  },
+
+  {
+    title:
+      "Elmano de Freitas divulga primeiros nomes do secretariado; veja lista",
+    text: "Governador eleito anunciou os primeiros sete titulares de pastas, sendo quatro homens e três mulheres",
+    banner:
+      "https://www.opovo.com.br/_midias/jpg/2022/12/19/818x460/1_elmano_relatorio_da_transicao_22-20586692.jpg",
+  },
+
+  {
+    title:
+      "Governo Lula é imprevisível, e aumento de gastos preocupa, analisam economistas",
+    text: "PT assumirá com contas públicas melhores que na época de Dilma e não poderá dizer que há uma 'herança maldita' de Bolsonaro",
+    banner:
+      "https://img.r7.com/images/ae-lula-dedo-em-riste-1500-16122022094600088?crop_position=c&dimensions=340x191",
+  },
+];
