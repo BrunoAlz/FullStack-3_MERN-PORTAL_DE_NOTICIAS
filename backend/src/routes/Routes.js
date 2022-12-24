6fa80a9add332849bed6b4d416905a8054a12585
Bruno Alves <bruno6821@gmail.com>
2022-12-23 21:36:05 -0300

    Implementando a configuração do Swagger API

--- backend/src/routes/Routes.js
+++ backend/src/routes/Routes.js
@@ -5,7 +5,7 @@ const router = express();
 router.use("/Api/Users", require("./userRoutes")); // 4° Importando as Rotas do Usuário:
 router.use("/Api/Users/Auth", require("./authRoutes"));
 router.use("/Api/Posts", require("./newsRoutes")); 
-router.use("/Api/Documentacao", require("./swaggerRouter")); 
+router.use("/", require("./swaggerRouter")); 
 
 router.get("/", (req, res) => {
   res.send("API WORKING");
