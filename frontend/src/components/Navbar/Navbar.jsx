324f4b447e692b99551b9dce20d1c4060864f0e7
Bruno Alves <bruno6821@gmail.com>
2022-12-24 11:57:08 -0300

    Criando o Component nav

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="input-search-space">
+          <i className="bi bi-search"></i>
+          <input type="text" placeholder="Pesquise por um título" />
+        </div>
+
+        {/* <img src={logo} alt="Logo do Breaking News" /> */}
+
+        <button>Entrar</button>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
