570e8507ca8038d92c80ddbf14da0a76deb1adce
Bruno Alves <bruno6821@gmail.com>
2022-12-23 21:11:40 -0300

    Adicionando a Rota da Documentação

--- backend/src/routes/Routes.js
+++ backend/src/routes/Routes.js
@@ -3,8 +3,9 @@ const router = express();
 
 // Aqui serão importadas as Rotas das "APIs"
 router.use("/Api/Users", require("./userRoutes")); // 4° Importando as Rotas do Usuário:
-router.use("/Api/Users/Auth", require("./authRoutes")); // 5° Importando as Rotas de Authenticação:
-router.use("/Api/Posts", require("./newsRoutes")); // 5° Importando as Rotas de Authenticação:
+router.use("/Api/Users/Auth", require("./authRoutes"));
+router.use("/Api/Posts", require("./newsRoutes")); 
+router.use("/Api/Documentacao", require("./swaggerRouter")); 
 
 router.get("/", (req, res) => {
   res.send("API WORKING");
