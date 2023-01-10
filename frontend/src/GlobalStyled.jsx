21bd563b8254a8622c680c888343b13ba4fd9173
Bruno Alves <bruno6821@gmail.com>
2023-01-10 13:45:33 -0300

    Rafctor: Componentizando os CSS globais

---
+++ frontend/src/GlobalStyled.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Newsreader:ital,opsz,wght@0,6..72,200;0,6..72,300;0,6..72,400;0,6..72,500;0,6..72,600;0,6..72,700;1,6..72,200;1,6..72,300;1,6..72,500;1,6..72,600&family=Noto+Sans:ital,wght@0,100;0,300;0,400;0,600;0,700;1,200;1,300;1,600&family=Roboto:ital,wght@0,300;1,100;1,300&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif
+}
+
+hrml {
+  width: auto;
+}
+
+body {
+  min-width: 100vw;
+  height: 100vw;
+  background-color: #f5f5f5
+}
+
+`;
